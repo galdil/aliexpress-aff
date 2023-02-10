e1fd23988e2688b2137a5c00411950a7454d8cbe
galdil <galdil@gmail.com>
2023-02-10 15:29:05 +0200

    fixes

--- src/Components/Products/Product.tsx
+++ src/Components/Products/Product.tsx
@@ -1,9 +1,11 @@
+import './products.css';
+
 const Product = ({ img, title, link }
 : { img: string, title: string, link: string }): JSX.Element => (
-  <div>
+  <div className="product-wrapper">
     <span>{title}</span>
-    <img alt="product" src={img} />
-    <a href={link}>Buy now</a>
+    <a href={link}><img className="product-img" alt="product" src={img} /></a>
+    <a className="cta" href={link}>Buy Now!</a>
   </div>
 );
 
--- src/Components/Products/Products.tsx
+++ src/Components/Products/Products.tsx
@@ -1,10 +1,12 @@
 import ProductList from './productsList';
 import Product from './Product';
 
+import './products.css';
+
 const Products = (): JSX.Element => (
-  <>
+  <div className="products-container">
     {ProductList.map((p) => <Product img={p.img} title={p.title} link={p.link} key={p.id} />)}
-  </>
+  </div>
 );
 
 export default Products;
