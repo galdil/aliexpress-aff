b93c3a91d6d63f20cbfe5b7215c3391a562c385b
galdil <galdil@gmail.com>
2023-02-10 15:35:33 +0200

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@Pages': "src/Pages",
-      '@Components': "src/Components",
+      '@Pages': path.resolve(__dirname, './src/Pages'),
+      '@Components': path.resolve(__dirname, './src/Components'),
     },
   }
 })
