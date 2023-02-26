43bc7f3efa0636a881968be7810528b618d7f58a
galdil <galdil@gmail.com>
2023-02-26 14:23:32 +0200

    Add power bank product

--- src/Components/Products/productsList.tsx
+++ src/Components/Products/productsList.tsx
@@ -1,8 +1,17 @@
-const products = [{
-  id: 1,
-  title: 'Professional Tripod',
-  link: 'https://s.click.aliexpress.com/e/_DFRZOE5',
-  img: 'https://ae01.alicdn.com/kf/Sb7c068d7f5d04575aa6650fbe0eca1e92/NA3560-55in-Stand-Xiaomi-HUAWEI.jpg_640x640.jpg',
-}];
+const products = [
+  {
+    id: 1,
+    title: 'Professional Tripod',
+    link: 'https://s.click.aliexpress.com/e/_DFRZOE5',
+    img: 'https://ae01.alicdn.com/kf/Sb7c068d7f5d04575aa6650fbe0eca1e92/NA3560-55in-Stand-Xiaomi-HUAWEI.jpg_640x640.jpg',
+  },
+  {
+    id: 2,
+    title: 'Car Power Bank',
+    link: 'https://s.click.aliexpress.com/e/_DkilfgV',
+    img: 'https://ae01.alicdn.com/kf/S55946d2f6d634d9d842b333801cd9bc4S/Baseus-20000mAh-Car-Jump-Starter-Power-Bank-2000A-10000mAh-Car-Battery-Charger-Auto-Emergency-Booster-Starting.png_.webp',
+  },
+
+];
 
 export default products;
